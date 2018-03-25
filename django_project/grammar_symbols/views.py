e07a55aa758ab12654e43dde7464dec9d96a4582
sandman27 <jackson@fellowsfamily.com>
2018-03-25 13:41:48 -0700

    update ui to fill multiple rows

--- django_project/grammar_symbols/views.py
+++ django_project/grammar_symbols/views.py
@@ -5,6 +5,6 @@ import spacy
 nlp = spacy.load('en')
 
 def index(request):
-	doc = nlp("Jackson is, of course, better than Grant.")
+	doc = nlp("It is reaching out for a better state of society than now exists...a craving for intellectual and moral growth; it is a longing to interpret the laws of creation; it means a wish for less misery among the poor, less ignorance in schools, less bigotry in temple, less suffering in the hospital, less fraud in business, less folly in politics...more study of nature, more love of art, more lessons from history, more security in property, more health in cities, more virtue in country, more wisdom in legislation, more intelligence, more happiness.")
 	words = [{'token': token, 'file': 'grammar_symbols/{0}.svg'.format(token.pos_)} for token in doc]
 	return render(request, 'grammar_symbols/index.html', {'words': words})
